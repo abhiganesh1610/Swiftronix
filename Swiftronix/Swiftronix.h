90615cd54ddcc4c77a0ec643869be49e6d53b749
Ganesh <ganesh@zeoner.com>
2024-12-27 12:28:04 +0530

    Initial Commit

---
+++ Swiftronix/Swiftronix.h
@@ -0,0 +1,18 @@
+//
+//  Swiftronix.h
+//  Swiftronix
+//
+//  Created by Ganesh on 27/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swiftronix.
+FOUNDATION_EXPORT double SwiftronixVersionNumber;
+
+//! Project version string for Swiftronix.
+FOUNDATION_EXPORT const unsigned char SwiftronixVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiftronix/PublicHeader.h>
+
+
---
+++ SwiftronixTests/SwiftronixTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftronixTests.swift
+//  SwiftronixTests
+//
+//  Created by Ganesh on 27/12/24.
+//
+
+import Testing
+@testable import Swiftronix
+
+struct SwiftronixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
